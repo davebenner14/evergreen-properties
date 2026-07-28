9cea32526584b779c2ad72ae5e115912b6c56a71
David Benner <119465113+davebenner14@users.noreply.github.com>
2026-07-28 13:37:24 -0400

    Improve local news image handling and fallbacks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,6 +12,15 @@ function Navbar() {
         <Link to="/apply">Apply</Link>
         <Link to="/maintenance">Maintenance</Link>
         <Link to="/contact">Contact</Link>
+
+        <a
+          href="https://airtable.com/appmZFilPYR2vMKWB/pagsktftiTWwsFaFv?TGwni=b%3AWzAsWyJKR2NzaCIsMCx0cnVlLCJUR0kwdiJdXQ&Q8oQh=b%3AWzAsWyJKR2NzaCIsMCxudWxsLCJiYmVjNiJdXQ&Qniw5=b%3AWzAsWyJWRmFrWSIsOSxbInNlbFFmT3ptODFQNHZ4SFA5Iiwic2Vsbmk5bnh3SW1FVlptN3EiLCJzZWxKQjU3N1gxY3g2b3hubSJdLCJoWUg2OCJdXQ&BrXPW=b%3AWzAsWyJjMGVZayIsOSxbInNlbDc3TmFZV0RXMlRLeVBhIiwic2VsSU53T3FRM2ExamFVVEYiLCJzZWxTZXNSQzhJdWY2M1Y2YSJdLCJqN3lSTyJdXQ"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="staffLogin"
+        >
+          Staff Login
+        </a>
       </div>
     </nav>
   );
